7d5e2f8c45af03da510cf89457e1810b32d60bac
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-03-09 06:13:19 +0700

    benerin tampilan

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -2,13 +2,15 @@
 
 @section('content')
     <div
-        class="relative min-h-screen bg-gray-100 bg-center sm:flex sm:justify-center sm:items-center bg-dots dark:bg-gray-900 selection:bg-indigo-500 selection:text-white">
+        class="">
         <div>
             @livewire('welcome.header')
         </div>
         <div>
             @livewire('todo.todo-form')
         </div>
-        @livewire('todo.todo-list')
+        <div>
+            @livewire('todo.todo-list')
+        </div>
     </div>
 @endsection
