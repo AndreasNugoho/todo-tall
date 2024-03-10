be413aa07278aadcc351595f7376331e1eed3af0
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-03-10 08:13:43 +0700

    search bar

--- resources/views/livewire/todo/todo-pending.blade.php
+++ resources/views/livewire/todo/todo-pending.blade.php
@@ -8,7 +8,7 @@
             On Progress</h1>
 
         {{-- Date --}}
-        <div class="inline-flex py-10">
+        <div class="inline-flex py-10 border border-black">
             <div x-data="{
                 datePickerOpen: false,
                 datePickerValue: '',
@@ -367,14 +367,26 @@
                 </div>
             </div>
 
-
-
         </div>
         <button type="button"
             class="relative inline-flex items-center justify-center px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 rounded-md top-[34px] bg-neutral-950 hover:bg-neutral-900 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none"
             wire:click="resetDate()">
             Reset
         </button>
+        <div class="inline-flex items-center relative">
+            <span class="absolute">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-5 h-5 mx-3 text-gray-400 dark:text-gray-600">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
+            </span>
+
+            <input type="text" placeholder="Search"
+                class="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+        </div>
+
+
         {{-- End Date --}}
 
 
